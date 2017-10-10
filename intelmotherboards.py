cb454ae40b8125d10fa85b6166dfb98d261d5a44
Jared Beach <jaredbeachdesign@gmail.com>
2017-10-10 07:14:22 -0500

    fix intelmotherboards

--- intelmotherboards.py
+++ intelmotherboards.py
@@ -2,11 +2,11 @@ import lxml.html
 import requests
 from neweggpy.nefuncs import IterPages,BoolToInt,getPIDS,getData,insertData
 
-baseurl = 'http://m.newegg.com/ProductList?description=Intel+Motherboards' + \
-          '&categoryId=280&storeId=1&nodeId=7627&parentCategoryId=20' + \
-          '&isSubCategory=true&categoryType=1'
+baseurl = 'https://m.newegg.com/ProductList?description=FHLEsA70dKzKoDqR2lBeblLSvjuwGkdtB%252fdjJTC%252f8VU%253d&storeid=1&categoryid=-1&nodeid=7627&storetype=2&subcategoryid=280&brandid=-1&nvalue=100007627&showseealldeals=False&itemcount=0&issubcategory=true&level=3' 
 
-pg1 = requests.get(baseurl).content
+headers = {'User-Agent': 'Mozilla/5.0 (Linux; U; Android 4.0.3; ko-kr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'}
+
+pg1 = requests.get(baseurl, headers=headers).content
 root1 = lxml.html.fromstring(pg1)
 page_count = IterPages(root1)
 URLs = ['%s&Page=%s' % (baseurl, pgnum) for pgnum in range(1, page_count + 1)]
